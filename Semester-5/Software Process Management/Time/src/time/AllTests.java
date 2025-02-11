55c1c96d41061139680290c6e773779ae111f2ed
Shrey <janishre@sheridancollege.ca>
2025-02-11 15:24:49 -0500

    commit

---
+++ Semester-5/Software Process Management/Time/src/time/AllTests.java
@@ -0,0 +1,10 @@
+package time;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ TimeTest.class })
+public class AllTests {
+
+}
---
+++ Semester-5/Software Process Management/Time/src/time/Hello.java
@@ -0,0 +1,9 @@
+package time;
+
+public class Hello {
+	public static void main(String[] args) {
+        System.out.println("Hello Jenkins");
+    }
+}
+
+///Users/shrey1807/Semester-5/Software Process Management/Time
